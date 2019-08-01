40bf9b92d98e1866b0fa7ea3d2ecdf8be7f13e57
dannyvidal <vidaldanny94@gmail.com>
2019-08-01 13:33:04 -0400

    added s to http

--- miracle-messages/src/components/services/ipLocation.js
+++ miracle-messages/src/components/services/ipLocation.js
@@ -1,4 +1,4 @@
 import axios from "axios";
 export async function getIP() {
-  return await axios.get(`http://api.ipstack.com/check?access_key=${process.env.REACT_APP_IP_STACK_API}`);
+  return await axios.get(`https://api.ipstack.com/check?access_key=${process.env.REACT_APP_IP_STACK_API}`);
 }
